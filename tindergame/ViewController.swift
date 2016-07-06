6f6274030571a7f84abef1d470107f97ca1fee13
Angeline Rao <angierao@fb.com>
2016-07-05 19:21:39 -0700

    Initial Commit

---
+++ tindergame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tindergame
+//
+//  Created by Angeline Rao on 7/5/16.
+//  Copyright © 2016 Angeline Rao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
